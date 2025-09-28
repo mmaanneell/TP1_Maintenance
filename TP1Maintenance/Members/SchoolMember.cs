19799fac99856de78a0ad9f60442366870423514
mmaanneell <150070696+mmaanneell@users.noreply.github.com>
2025-09-28 19:50:53 -0400

    Changement de dossier de la classe et refactor

---
+++ TP1Maintenance/Members/SchoolMember.cs
@@ -0,0 +1,16 @@
+﻿namespace SchoolManager
+{
+    public class SchoolMember
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+
+        public SchoolMember(string name = "", string address = "", int phone = 0)
+        {
+            Name = name;
+            Address = address;
+            Phone = phone;
+        }
+    }
+}
