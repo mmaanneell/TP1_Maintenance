ec8d5debfdddc7f05a8286dcc6516b5bf3000bdf
SamenthaL <2386297@bdeb.qc.ca>
2025-10-02 10:54:46 -0400

    Revert "Refactor / Principal"

--- TP1Maintenance/Members/Principal.cs
+++ TP1Maintenance/Members/Principal.cs
@@ -12,7 +12,7 @@ namespace Members
 
         public override void Display()
         {
-            Console.WriteLine($"Name: {Name}, Address: {Address}, PhoneNumber: {PhoneNumber}");
+            Console.WriteLine($"Name: {Name}, Address: {Address}, phoneNumber: {PhoneNumber}");
         }
     }
 }
