8205e4b1c69113909cdc7b7fb18cec47e0408f0d
Nadine <2344893@bdeb.qc.ca>
2025-10-05 15:01:21 -0400

    Supprimer les validations déjà présentes dans la classe SchoolMember

--- TP1Maintenance/Members/Employee.cs
+++ TP1Maintenance/Members/Employee.cs
@@ -12,33 +12,6 @@ namespace Members
         public Employee(string name, string address, int phoneNumber, int income)
         : base(name, address, phoneNumber)
         {
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                Console.WriteLine("Nom vide, nom par defaut utilise");
-                name = "Nom par defaut";
-            }
-            else
-            {
-                Name = name.Trim();
-            }
-            if (string.IsNullOrWhiteSpace(address))
-            {
-                Console.WriteLine("Adresse vide, adresse par defaut utilise");
-                Address = "Adresse par defaut";
-            }
-            else
-            {
-                Address = address.Trim();
-            }
-            if (phoneNumber <= 0)
-            {
-                Console.WriteLine("Le numero de telephone doit etre positif. Numero par defaut utilise");
-                PhoneNumber = 123456789;
-            }
-            else
-            {
-                PhoneNumber = phoneNumber;
-            }
             if (income < 0)
             {
                 Console.WriteLine("Income doit etre positif.");
