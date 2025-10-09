118df57498b2fe5f54e65bb9121524dc84ed47cf
Nadine <2344893@bdeb.qc.ca>
2025-10-09 11:51:19 -0400

    Ajout des tests unitaires pour le constructeur de Student

--- TP1MaintenanceTests/StudentTests.cs
+++ TP1MaintenanceTests/StudentTests.cs
@@ -5,5 +5,46 @@ namespace TP1MaintenanceTests;
 
 public class StudentTests
 {
-   
+    [Fact]
+    public void Constructor_WithValidGrade()
+    {
+
+        string name = "Nadine";
+        string address = "Montreal";
+        int phoneNumber = 5551234;
+        int grade = 85;
+
+        Student student = new Student(name, address, phoneNumber, grade);
+
+        Assert.Equal(name, student.Name);
+        Assert.Equal(address, student.Address);
+        Assert.Equal(phoneNumber, student.PhoneNumber);
+        Assert.Equal(grade, student.Grade);
+    }
+
+    [Fact]
+    public void Constructor_ShouldAddStudentToList()
+    {
+
+        Student.Students.Clear(); // Nettoyer la liste avant test
+        int initialCount = Student.Students.Count;
+
+        Student student = new Student("Sam", "Repentigny", 5550000, 90);
+
+        Assert.Contains(student, Student.Students);
+        Assert.Equal(initialCount + 1, Student.Students.Count);
+    }
+
+    [Fact]
+    public void Constructor_WithInvalidGrade()
+    {
+        string name = "Manel";
+        string address = "Laval";
+        int phoneNumber = 9999999;
+        int invalidGrade = -5;
+
+        Student student = new Student(name, address, phoneNumber, invalidGrade);
+
+        Assert.Equal(0, student.Grade);
+    }
 }
