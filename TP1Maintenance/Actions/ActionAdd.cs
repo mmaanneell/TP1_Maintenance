05494e330c9cc093afdcac2377e37448d70fe70d
SamenthaL <152306041+SamenthaL@users.noreply.github.com>
2025-10-12 16:29:05 -0400

    [Update] Deplacement de AddData() dans ActionAdd.cs

--- TP1Maintenance/Actions/ActionAdd.cs
+++ TP1Maintenance/Actions/ActionAdd.cs
@@ -102,4 +102,22 @@ public class ActionAdd : IActions
     }
 
 
+    public static void AddData()
+    {
+        Receptionist.receptionist.ComplaintRaised += (sender, complaint) => complaint.DisplayConfirmation();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            // Génération d'une liste d'étudiants et de professeurs pour ne pas avoir une liste vide au lancement du programme
+            int minGrade = JSONConfigurationManager.GradeSettings?.MinGrade ?? 0;
+            int maxGrade = JSONConfigurationManager.GradeSettings?.MaxGrade ?? 100;
+            Random randomRange = new Random();
+            
+            new Student($"Student{i}", $"Address{i}", 123456000 + i, randomRange.Next(minGrade, maxGrade + 1));
+            new Teacher($"Teacher{i}", $"Address{i}", 123457000 + i, subject: $"Subject{i}");
+            
+        }
+    }
+
+
 }
--- TP1Maintenance/Program.cs
+++ TP1Maintenance/Program.cs
@@ -5,27 +5,12 @@ using Microsoft.Extensions.Configuration;
 
 public class Program
 {
-    private static void AddData()
-    {
-        Receptionist.receptionist.ComplaintRaised += (sender, complaint) => complaint.DisplayConfirmation();
 
-        for (int i = 1; i <= 10; i++)
-        {
-            // Génération d'une liste d'étudiants et de professeurs pour ne pas avoir une liste vide au lancement du programme
-            int minGrade = JSONConfigurationManager.GradeSettings?.MinGrade ?? 0;
-            int maxGrade = JSONConfigurationManager.GradeSettings?.MaxGrade ?? 100;
-            Random randomRange = new Random();
-            
-            new Student($"Student{i}", $"Address{i}", 123456000 + i, randomRange.Next(minGrade, maxGrade + 1));
-            new Teacher($"Teacher{i}", $"Address{i}", 123457000 + i, subject: $"Subject{i}");
-            
-        }
-    }
 
     public static void Main(string[] args)
     {
         JSONConfigurationManager.Initialize();
-        AddData();
+        ActionAdd.AddData();
 
         Console.WriteLine("-------------- Welcome ---------------\n");
 
