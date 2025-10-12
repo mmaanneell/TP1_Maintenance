9acae04e46055ffdf830336f9971577bfbd5c4ee
SamenthaL <152306041+SamenthaL@users.noreply.github.com>
2025-10-12 16:25:10 -0400

    [Correction] Bug Display a la source de DataAdd

--- TP1Maintenance/Program.cs
+++ TP1Maintenance/Program.cs
@@ -13,8 +13,12 @@ public class Program
         {
             // Génération d'une liste d'étudiants et de professeurs pour ne pas avoir une liste vide au lancement du programme
             int minGrade = JSONConfigurationManager.GradeSettings?.MinGrade ?? 0;
-            Student.Students.Add(new Student($"Student{i}", $"Address{i}", 123456000 + i, i + minGrade)); 
-            Teacher.Teachers.Add(new Teacher($"Teacher{i}", $"Address{i}", 123457000 + i, subject: $"Subject{i}"));
+            int maxGrade = JSONConfigurationManager.GradeSettings?.MaxGrade ?? 100;
+            Random randomRange = new Random();
+            
+            new Student($"Student{i}", $"Address{i}", 123456000 + i, randomRange.Next(minGrade, maxGrade + 1));
+            new Teacher($"Teacher{i}", $"Address{i}", 123457000 + i, subject: $"Subject{i}");
+            
         }
     }
 
