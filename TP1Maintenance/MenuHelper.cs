f4df72d2888c05d29bf5b32ccca8f0cf804c3cc8
SamenthaL <152306041+SamenthaL@users.noreply.github.com>
2025-10-12 15:28:31 -0400

    [Update] NullCheck Acceptchoice() & AcceptMemberType()

--- TP1Maintenance/MenuHelper.cs
+++ TP1Maintenance/MenuHelper.cs
@@ -5,19 +5,21 @@ public static class MenuHelper
 {
     public static int AcceptChoices()
     {
-        return AskNumberInput("\n1. Add\n2. Display\n3. Pay\n4. Raise Complaint\n5. Student Performance\n6. Undo Last Action\nPlease enter action type: ");
+        string question = VerifyQuestion("\n1. Add\n2. Display\n3. Pay\n4. Raise Complaint\n5. Student Performance\n6. Undo Last Action\nPlease enter action type: ");
+        return AskNumberInput(question);
     }
 
     public static int AcceptMemberType()
     {
-        int x = AskNumberInput("\n0. Principal\n1. Teacher\n2. Student\n3. Receptionist\nPlease enter the member type: ");
+        string question = VerifyQuestion("\n0. Principal\n1. Teacher\n2. Student\n3. Receptionist\nPlease enter the member type: ");
+        int x = AskNumberInput(question);
         return Enum.IsDefined(typeof(SchoolMemberType), x) ? x : -1;
     }
 
     public static string AskInfoInput(string question)
     {
         Console.WriteLine(question);
-        return Console.ReadLine();
+        return Console.ReadLine() ?? string.Empty;
     }
 
     public static int AskNumberInput(string question)
@@ -32,5 +34,16 @@ public static class MenuHelper
 
         return result;
     }
+
+    public static string VerifyQuestion(string question)
+    {
+        if (string.IsNullOrEmpty(question))
+        {
+            Console.WriteLine("AskInfoInput requires a non-null question string.\nPlease provide a valid question then restart the program.");
+            Environment.Exit(1);
+        }
+        return AskInfoInput(question);
+    }
+
 }
 
